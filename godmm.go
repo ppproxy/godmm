0fa1d887d2def3ab5ff5f9f6d24ac9b82aa6af2f
Jun Zhou <junz@junz.info>
2016-03-11 15:31:55 -0800

    gofmt

--- godmm.go
+++ godmm.go
@@ -1,53 +1,53 @@
 package main
 
 import (
-  "encoding/json"
-  "flag"
-  "fmt"
-  "time"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"time"
 
-  "github.com/golang/glog"
-  "github.com/junzh0u/opendmm"
+	"github.com/golang/glog"
+	"github.com/junzh0u/opendmm"
 )
 
 func search(query string, dbpath string) {
-  db, err := opendmm.NewDB(dbpath)
-  if err != nil {
-    glog.Fatal(err)
-  }
-  defer db.Close()
-  metach := opendmm.Search(query, db)
-  select {
-  case meta, ok := <-metach:
-    if ok {
-      metajson, _ := json.MarshalIndent(meta, "", "  ")
-      fmt.Println(string(metajson))
-    } else {
-      glog.Exit("Not found")
-    }
-  case <-time.After(10 * time.Second):
-    glog.Fatal("Timeout")
-  }
+	db, err := opendmm.NewDB(dbpath)
+	if err != nil {
+		glog.Fatal(err)
+	}
+	defer db.Close()
+	metach := opendmm.Search(query, db)
+	select {
+	case meta, ok := <-metach:
+		if ok {
+			metajson, _ := json.MarshalIndent(meta, "", "  ")
+			fmt.Println(string(metajson))
+		} else {
+			glog.Exit("Not found")
+		}
+	case <-time.After(10 * time.Second):
+		glog.Fatal("Timeout")
+	}
 }
 
 func crawl(dbpath string) {
-  db, err := opendmm.NewDB(dbpath)
-  if err != nil {
-    glog.Fatal(err)
-  }
-  opendmm.Crawl(db)
+	db, err := opendmm.NewDB(dbpath)
+	if err != nil {
+		glog.Fatal(err)
+	}
+	opendmm.Crawl(db)
 }
 
 func main() {
-  flag.Set("stderrthreshold", "FATAL")
-  dbpath := flag.String("db", "/tmp/opendmm.boltdb", "path for crawler db")
-  flag.Parse()
-  switch flag.Arg(0) {
-  case "search":
-    search(flag.Arg(1), *dbpath)
-  case "crawl":
-    crawl(*dbpath)
-  default:
-    search(flag.Arg(0), *dbpath)
-  }
+	flag.Set("stderrthreshold", "FATAL")
+	dbpath := flag.String("db", "/tmp/opendmm.boltdb", "path for crawler db")
+	flag.Parse()
+	switch flag.Arg(0) {
+	case "search":
+		search(flag.Arg(1), *dbpath)
+	case "crawl":
+		crawl(*dbpath)
+	default:
+		search(flag.Arg(0), *dbpath)
+	}
 }
